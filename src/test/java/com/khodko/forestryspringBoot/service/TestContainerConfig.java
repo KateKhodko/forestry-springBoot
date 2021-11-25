3666526b298381a5f832a1fddbf0ccbded581f07
Yan Traulka <Traulko@lanit.ru>
2021-11-25 12:26:08 +0300

    correct mistakes

---
+++ src/test/java/com/khodko/forestryspringBoot/service/TestContainerConfig.java
@@ -0,0 +1,29 @@
+package com.khodko.forestryspringBoot.service;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
+public abstract class TestContainerConfig {
+
+    private static final String IMAGE_VERSION = "postgres:11.1";
+    public static PostgreSQLContainer<?> postgreDBContainer;
+
+    static {
+        postgreDBContainer = new PostgreSQLContainer<>(IMAGE_VERSION);
+        postgreDBContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreDBContainer::getJdbcUrl);
+        registry.add("spring.datasource.password", postgreDBContainer::getPassword);
+        registry.add("spring.datasource.username", postgreDBContainer::getUsername);
+    }
+}
